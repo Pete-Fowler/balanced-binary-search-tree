7f29ea9052aa760206ea4d16b0643ef68018f4dc
Pete Fowler <fowler.pete@gmail.com>
2022-11-29 10:22:45 -0700

    Complete buildTree()

--- src/Tree.js
+++ src/Tree.js
@@ -2,29 +2,36 @@ import Node from "./Node.js";
 
 export default class Tree {
   constructor(array) {
-    this.array = array;
+    const set = new Set(array);
+    const arr = Array.from(set);
+    arr.sort();
+    this.array = arr;
     this.root = null;
   }
 
   buildTree(array = this.array, start = 0, end = this.array.length - 1) {
-    const set = new Set(array);
-    const arr = Array.from(set);
-    arr.sort();
-    const root = new Node()
-    console.log(arr, this.root);
-    return this.root;
+    if (start > end) return null;
+
+    let mid = (start + end) / 2;
+    const root = new Node(array[mid]);
+
+    root.left = this.buildTree(array, start, mid - 1);
+    root.right = this.buildTree(array, mid + 1, end);
+
+    return root;
   }
-  /*
-Write a buildTree function which takes an array of data (e.g. [1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324]) and turns it into a balanced binary tree full of Node objects appropriately placed (don’t forget to sort and remove duplicates!). The buildTree function should return the level-0 root node.
-  */
 
   prettyPrint(node, prefix = "", isLeft = true) {
     if (node.right !== null) {
-      prettyPrint(node.right, `${prefix}${isLeft ? "│   " : "    "}`, false);
+      this.prettyPrint(
+        node.right,
+        `${prefix}${isLeft ? "│   " : "    "}`,
+        false
+      );
     }
     console.log(`${prefix}${isLeft ? "└── " : "┌── "}${node.data}`);
     if (node.left !== null) {
-      prettyPrint(node.left, `${prefix}${isLeft ? "    " : "│   "}`, true);
+      this.prettyPrint(node.left, `${prefix}${isLeft ? "    " : "│   "}`, true);
     }
   }
 }
--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import Tree from "./Tree.js";
 
-const t = new Tree([1, 2, 3, 4, 5]);
-t.buildTree();
+const t = new Tree([6, 7, 1, 2, 3, 4, 5]);
+const root = t.buildTree();
+t.prettyPrint(root);
