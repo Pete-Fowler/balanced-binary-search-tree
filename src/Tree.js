1eeea76ec58b6e0620d515d4f12ecaee0893a2ba
Pete Fowler <fowler.pete@gmail.com>
2022-11-29 09:58:57 -0700

    Edit parameters

--- src/Tree.js
+++ src/Tree.js
@@ -1,12 +1,22 @@
+import Node from "./Node.js";
+
 export default class Tree {
   constructor(array) {
     this.array = array;
-    this.root = buildTree(array);
+    this.root = null;
   }
 
-  buildTree(array) {
-    const middle = this.array;
+  buildTree(array = this.array, start = 0, end = this.array.length - 1) {
+    const set = new Set(array);
+    const arr = Array.from(set);
+    arr.sort();
+    const root = new Node()
+    console.log(arr, this.root);
+    return this.root;
   }
+  /*
+Write a buildTree function which takes an array of data (e.g. [1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324]) and turns it into a balanced binary tree full of Node objects appropriately placed (don’t forget to sort and remove duplicates!). The buildTree function should return the level-0 root node.
+  */
 
   prettyPrint(node, prefix = "", isLeft = true) {
     if (node.right !== null) {
--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
 import Tree from "./Tree.js";
 
 const t = new Tree([1, 2, 3, 4, 5]);
-t.prettyPrint(t.root);
+t.buildTree();
