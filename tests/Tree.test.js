fe4fa3e4ee0ddce0e902767127f96a66d9b707b1
Pete Fowler <fowler.pete@gmail.com>
2022-12-01 12:52:16 -0700

    Add inOrder test

--- tests/Tree.test.js
+++ tests/Tree.test.js
@@ -194,4 +194,26 @@ describe("Tree", () => {
     expect(spy).toHaveBeenNthCalledWith(7, 7);
     spy.mockRestore();
   });
+  it("levelOrder returns an array of values if given no callback", () => {
+    expect(tree.levelOrder()).toEqual([4, 2, 6, 1, 3, 5, 7]);
+  });
+  it("inOrder returns an array of values if given no callback", () => {
+    expect(tree.levelOrder()).toEqual([1, 2, 3, 4, 5, 6, 7]);
+  it("inOrder traverses appropriately", () => {
+    const spy = jest.spyOn(console, "log");
+    function cb(node) {
+      console.log(node.data);
+    }
+    tree.inOrder(cb);
+    expect(spy).toHaveBeenCalled();
+    expect(spy).toHaveBeenCalledTimes(7);
+    expect(spy).toHaveBeenNthCalledWith(1, 1);
+    expect(spy).toHaveBeenNthCalledWith(2, 2);
+    expect(spy).toHaveBeenNthCalledWith(3, 3);
+    expect(spy).toHaveBeenNthCalledWith(4, 4);
+    expect(spy).toHaveBeenNthCalledWith(5, 5);
+    expect(spy).toHaveBeenNthCalledWith(6, 6);
+    expect(spy).toHaveBeenNthCalledWith(7, 7);
+    spy.mockRestore();
+  });
 });
