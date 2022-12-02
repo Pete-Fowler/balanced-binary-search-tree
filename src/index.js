eb0cd9ea440ded3ae401ffcca929b2cf6e052fea
Pete Fowler <fowler.pete@gmail.com>
2022-12-01 19:21:16 -0700

    Complete driver script

--- src/index.js
+++ src/index.js
@@ -1,7 +1,5 @@
 import Tree from "./Tree.js";
 
-const t = new Tree([6, 6, 7, 1, 2, 3, 4, 5]);
-
 function randomArray(length) {
   const array = [];
   for (let i = 0; i < length; i++) {
@@ -10,4 +8,42 @@ function randomArray(length) {
   return array;
 }
 
+const t = new Tree(randomArray(10));
 
+console.log(t.isBalanced() ? "Tree is balanced" : "Tree is not balanced");
+
+t.prettyPrint();
+
+console.log("Inorder traversal:");
+t.inOrder((node) => console.log(node.data));
+
+console.log("Level order traversal:");
+t.levelOrder((node) => console.log(node.data));
+
+console.log("Preorder traversal:");
+t.preOrder((node) => console.log(node.data));
+
+console.log("Postorder traversal:");
+t.postOrder((node) => console.log(node.data));
+
+t.insert(100);
+t.insert(110);
+t.insert(105);
+t.insert(115);
+t.insert(120);
+
+console.log(
+  "Inserted 5 nodes. " +
+    `Tree is now ${t.isBalanced() ? "balanced." : "unbalanced."}`
+);
+
+t.prettyPrint();
+
+t.rebalance();
+
+console.log(
+  "Rebalanced tree. " +
+    `Tree is now ${t.isBalanced ? "balanced." : "unbalanced."}`
+);
+
+t.prettyPrint();
